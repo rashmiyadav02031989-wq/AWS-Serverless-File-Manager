4ce011a66aee99349dd55f9467bcf881954082ee
Rashmi Yadav <rashmiyadav02031989@gmail.com>
2026-06-17 17:04:33 -0500

    Add files via upload

---
+++ lambda/delete-file-lambda.py
@@ -0,0 +1,44 @@
+import json
+import boto3
+import urllib.parse
+
+s3 = boto3.client("s3")
+BUCKET = "serverless-storage-app-bucket"
+
+def lambda_handler(event, context):
+
+    params = event.get("queryStringParameters") or {}
+    key = params.get("key")
+
+    if not key:
+        return {
+            "statusCode": 400,
+            "headers": {"Access-Control-Allow-Origin": "*"},
+            "body": json.dumps({"error": "Missing key"})
+        }
+
+    try:
+        # decode key (important)
+        key = urllib.parse.unquote(key)
+
+        s3.delete_object(
+            Bucket=BUCKET,
+            Key=key
+        )
+
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Access-Control-Allow-Origin": "*"
+            },
+            "body": json.dumps({"message": "Deleted successfully"})
+        }
+
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "headers": {
+                "Access-Control-Allow-Origin": "*"
+            },
+            "body": json.dumps({"error": str(e)})
+        }
